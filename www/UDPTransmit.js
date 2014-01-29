686351cfc2e8003d518413071f35e5057df45103
Jack Piccininni <applejacko@mac.com>
2014-01-29 16:47:01 -0600

    ccc

--- www/UDPTransmit.js
+++ www/UDPTransmit.js
@@ -1,22 +1,4 @@
-udptransmit = function(str, callback) {
-	cordova.exec(
-				 
-				 // success callback
-				 callback,
-				 
-				 // success callback
-				 function(err) {
-				 /* callback('UDP transmit failed.'); */
-				 },
-				 
-				 // the OS-level service (class)
-				 "Datagram",
-				 
-				 // the OS-level action (method of the class)
-				 "send",
-				 
-				 // arguments (of the method)
-				 [str]
-				 
-				 );
-};
+cordova.define("edu.uic.updtransmit.udptransmit", function(require, exports, module) {// require, exports, module are predefined
+			   
+			   var exec = cordova.require('cordova/exec');
+});

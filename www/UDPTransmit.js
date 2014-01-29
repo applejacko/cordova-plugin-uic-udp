31c7d1cabb870faa4ad5b16bed6628b2c37f2d90
Jack Piccininni <applejacko@mac.com>
2014-01-29 16:39:53 -0600

    zzz

---
+++ www/UDPTransmit.js
@@ -0,0 +1,22 @@
+udptransmit = function(str, callback) {
+	cordova.exec(
+				 
+				 // success callback
+				 callback,
+				 
+				 // success callback
+				 function(err) {
+				 /* callback('UDP transmit failed.'); */
+				 },
+				 
+				 // the OS-level service (class)
+				 "Datagram",
+				 
+				 // the OS-level action (method of the class)
+				 "send",
+				 
+				 // arguments (of the method)
+				 [str]
+				 
+				 );
+};

6a1e2a6671c71f1dce4bc3bafe7fdd12978a4e2d
Jack Piccininni <applejacko@mac.com>
2014-01-29 16:13:55 -0600

    zzz

--- src/android/UDPTransmit.java
+++ src/android/UDPTransmit.java
@@ -1,19 +1,5 @@
 package edu.uic.udptransmit;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.MulticastSocket;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import android.util.Log;
-import android.util.SparseArray;
 
 public class UDPTransmit extends CordovaPlugin {
 	

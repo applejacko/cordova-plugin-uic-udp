8235709ac3347dfb4ddc3e438d145a1b62fccb28
Jack Piccininni <applejacko@mac.com>
2014-01-29 15:49:43 -0600

    More changes

--- www/UDPTransmit.js
+++ www/UDPTransmit.js
@@ -1,4 +1,4 @@
-cordova.define("edu.uic.udp", function(require, exports, module) {// require, exports, module are predefined
+cordova.define("edu.uic.udptransmit", function(require, exports, module) {// require, exports, module are predefined
 			   
 			   var exec = cordova.require('cordova/exec');
 			   

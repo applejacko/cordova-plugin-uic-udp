508fc95d433aec4a5bca539aa6de0a68002a9799
Jack Piccininni <applejacko@mac.com>
2014-01-30 10:59:42 -0600

    uncluttered

--- www/udptransmit.js
+++ www/udptransmit.js
@@ -2,16 +2,7 @@ var exec = require('cordova/exec');
 var platform = require('cordova/platform');
 
 module.exports = {
-	
-    /**
-     * Open a native alert dialog, with a customizable title and button text.
-     *
-     * @param {String} message              Message to print in the body of the alert
-     * @param {Function} completeCallback   The callback that is called when user clicks on a button.
-     * @param {String} title                Title of the alert dialog (default: Alert)
-     * @param {String} buttonLabel          Label of the close button (default: OK)
-     */
-alert: function(message, completeCallback, title, buttonLabel) {
-	exec(completeCallback, null, "Notification", "alert", [message, title, buttonLabel]);
+	alert: function(message, completeCallback, title, buttonLabel) {
+		exec(completeCallback, null, "Notification", "alert", [message, title, buttonLabel]);
 	}
 };

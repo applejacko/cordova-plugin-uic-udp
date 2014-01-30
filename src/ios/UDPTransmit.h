633bf06b17c38d51b45e24eeea2338b4028a3eb3
Jack Piccininni <applejacko@mac.com>
2014-01-30 13:33:48 -0600

    Seeing if still crashes

--- src/ios/UDPTransmit.h
+++ src/ios/UDPTransmit.h
@@ -1,6 +1,12 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioServices.h>
 #import <Cordova/CDVPlugin.h>
 
 @interface UDPTransmit : CDVPlugin
+	
+	- (void)alert:(CDVInvokedUrlCommand*)command;
 
 @end
 

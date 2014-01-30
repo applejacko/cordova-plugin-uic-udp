c1330805a3758e9bc299086f9cf75ed1fd82f512
Jack Piccininni <applejacko@mac.com>
2014-01-30 13:33:25 -0600

    Added more beef to see if this crashes still

--- src/ios/UDPTransmit.m
+++ src/ios/UDPTransmit.m
@@ -1,5 +1,42 @@
 #import "UDPTransmit.h"
 
 @implementation UDPTransmit
+	
+	- (void)showDialogWithMessage:(NSString*)message title:(NSString*)title buttons:(NSArray*)buttons defaultText:(NSString*)defaultText callbackId:(NSString*)callbackId dialogType:(NSString*)dialogType
+	{
+		CDVAlertView* alertView = [[CDVAlertView alloc]
+								   initWithTitle:title
+								   message:message
+								   delegate:self
+								   cancelButtonTitle:nil
+								   otherButtonTitles:nil];
+		
+		alertView.callbackId = callbackId;
+		
+		int count = [buttons count];
+		
+		for (int n = 0; n < count; n++) {
+			[alertView addButtonWithTitle:[buttons objectAtIndex:n]];
+		}
+		
+		if ([dialogType isEqualToString:DIALOG_TYPE_PROMPT]) {
+			alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+			UITextField* textField = [alertView textFieldAtIndex:0];
+			textField.text = defaultText;
+		}
+		
+		[alertView show];
+	}
+	
+- (void)alert:(CDVInvokedUrlCommand*)command
+	{
+		NSString* callbackId = command.callbackId;
+		NSString* message = [command argumentAtIndex:0];
+		NSString* title = [command argumentAtIndex:1];
+		NSString* buttons = [command argumentAtIndex:2];
+		
+		[self showDialogWithMessage:message title:title buttons:@[buttons] defaultText:nil callbackId:callbackId dialogType:DIALOG_TYPE_ALERT];
+	}
+
 
 @end

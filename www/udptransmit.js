dbd157f115496a88648484bddeca808a1c9d11da
Jack Piccininni <applejacko@mac.com>
2014-01-29 18:21:21 -0600

    window.alert

--- www/udptransmit.js
+++ www/udptransmit.js
@@ -1,4 +1,8 @@
 cordova.define("edu.uic.updtransmit.udptransmit", function(require, exports, module) {// require, exports, module are predefined
+
+			   var exec = cordova.require('cordova/exec');
 			   
-			   
+			   function TestFunc() {
+					window.alert("TestFunc");
+			   }
 });

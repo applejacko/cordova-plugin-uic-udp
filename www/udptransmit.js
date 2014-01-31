5521ebd9b04af83cf431f066c6b4abbea9267f25
Jack Piccininni <applejacko@mac.com>
2014-01-31 10:43:52 -0600

    Pared down to stupid simple skeleton that calls no native API, Android only.

--- www/udptransmit.js
+++ www/udptransmit.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 var platform = require('cordova/platform');
 
 module.exports = {
-	alert: function(message, completeCallback, title, buttonLabel) {
-		exec(completeCallback, null, "UDPTransmit", "alert", [message, title, buttonLabel]);
+	whatever: function() {
+		return 42;
 	}
 };

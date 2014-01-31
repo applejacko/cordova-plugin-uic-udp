4ef6e507993d9fee1978ffca621d3c21c8414143
Jack Piccininni <applejacko@mac.com>
2014-01-31 10:55:44 -0600

    And stupid simple with no native API calls for iOS.

--- src/ios/UDPTransmit.h
+++ src/ios/UDPTransmit.h
@@ -1,19 +1,7 @@
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <AudioToolbox/AudioServices.h>
 #import <Cordova/CDVPlugin.h>
 
 @interface UDPTransmit : CDVPlugin
 	
-	- (void)alert:(CDVInvokedUrlCommand*)command;
+	- (int)whatever:(CDVInvokedUrlCommand*)command;
 	
 	@end
-
-@interface CDVAlertView : UIAlertView {}
-	
-	@property (nonatomic, copy) NSString* callbackId;
-	
-	@end
-
-
--- src/ios/UDPTransmit.m
+++ src/ios/UDPTransmit.m
@@ -1,56 +1,10 @@
 #import "UDPTransmit.h"
 
-#import <Cordova/NSDictionary+Extensions.h>
-#import <Cordova/NSArray+Comparisons.h>
-
-#define DIALOG_TYPE_ALERT @"alert"
-#define DIALOG_TYPE_PROMPT @"prompt"
-
-
 @implementation UDPTransmit
 	
-	- (void)showDialogWithMessage:(NSString*)message title:(NSString*)title buttons:(NSArray*)buttons defaultText:(NSString*)defaultText callbackId:(NSString*)callbackId dialogType:(NSString*)dialogType
+	- (int)whatever:(CDVInvokedUrlCommand*)command
 	{
-		CDVAlertView* alertView = [[CDVAlertView alloc]
-								   initWithTitle:title
-								   message:message
-								   delegate:self
-								   cancelButtonTitle:nil
-								   otherButtonTitles:nil];
-		
-		alertView.callbackId = callbackId;
-		
-		int count = [buttons count];
-		
-		for (int n = 0; n < count; n++) {
-			[alertView addButtonWithTitle:[buttons objectAtIndex:n]];
-		}
-		
-		if ([dialogType isEqualToString:DIALOG_TYPE_PROMPT]) {
-			alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
-			UITextField* textField = [alertView textFieldAtIndex:0];
-			textField.text = defaultText;
-		}
-		
-		[alertView show];
+		return 42;
 	}
 	
-	- (void)alert:(CDVInvokedUrlCommand*)command
-	{
-		NSString* callbackId = command.callbackId;
-		NSString* message = [command argumentAtIndex:0];
-		NSString* title = [command argumentAtIndex:1];
-		NSString* buttons = [command argumentAtIndex:2];
-		
-		[self showDialogWithMessage:message title:title buttons:@[buttons] defaultText:nil callbackId:callbackId dialogType:DIALOG_TYPE_ALERT];
-	}
-	
-	
 	@end
-
-@implementation CDVAlertView
-	
-	@synthesize callbackId;
-	
-	@end
-

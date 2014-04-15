b50e2eabd60906ea13c94a962cc2242034212689
Jack Piccininni <applejacko@mac.com>
2014-04-15 16:55:54 -0500

    Trimmed the JS and iOS/Android to two calls.

--- www/udptransmit.js
+++ www/udptransmit.js
@@ -20,6 +20,7 @@ initialize: function(host, port) {
 				 [host, port]);
 	return true;
 },
+
 sendMessage: function(message) {
 	cordova.exec(
 				 function(){alert("success sending message");},
@@ -28,139 +29,7 @@ sendMessage: function(message) {
 				 "sendMessage",
 				 [message]);
 	return true;
-},
-createDatagramPacket: function(data, host, port) {
-	cordova.exec(
-				 function(){alert("success creating datagram packet");},
-				 function(error){alert("error creating datagram packet: " + error);},
-				 "UDPTransmit",
-				 "createDatagramPacket",
-				 [data, host, port]);
-	return true;
-},
-setDatagramPacketAddress: function(host) {
-	cordova.exec(
-				 function(){alert("success setting host address");},
-				 function(error){alert("error setting host address: " + error);},
-				 "UDPTransmit",
-				 "setDatagramPacketAddress",
-				 [host]);
-	return true;
-},
-getDatagramPacketAddress: function(callback) {
-	cordova.exec(
-				 callback,
-				 function(error){alert("error getting host address: " + error);},
-				 "UDPTransmit",
-				 "getDatagramPacketAddress",
-				 []);
-	return true;
-},
-setDatagramPacketPort: function(port) {
-	cordova.exec(
-				 function(){alert("success setting port");},
-				 function(error){alert("error setting port: " + error);},
-				 "UDPTransmit",
-				 "setDatagramPacketPort",
-				 [port]);
-	return true;
-},
-getDatagramPacketPort: function(callback) {
-	cordova.exec(
-				 callback,
-				 function(error){alert("error getting port: " + error);},
-				 "UDPTransmit",
-				 "getDatagramPacketPort",
-				 []);
-	return true;
-},
-setDatagramPacketData: function(host) {
-	cordova.exec(
-				 function(){alert("success setting packet data");},
-				 function(error){alert("error setting packet data: " + error);},
-				 "UDPTransmit",
-				 "setDatagramPacketData",
-				 [host]);
-	return true;
-},
-getDatagramPacketData: function(callback) {
-	cordova.exec(
-				 callback,
-				 function(error){alert("error getting packet data: " + error);},
-				 "UDPTransmit",
-				 "getDatagramPacketData",
-				 []);
-	return true;
-},
-createDatagramSocket: function() {
-	cordova.exec(
-				 function(){alert("success creating datagram socket");},
-				 function(error){alert("error creating datagram socket: " + error);},
-				 "UDPTransmit",
-				 "createDatagramSocket",
-				 []);
-	return true;
-},
-sendDatagramPacket: function() {
-	cordova.exec(
-				 function(){alert("success sending packet");},
-				 function(error){alert("error sending packet: " + error);},
-				 "UDPTransmit",
-				 "sendDatagramPacket",
-				 []);
-	return true;
-},
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	// This method has no interaction with the Native side
-whatever: function() {
-	return 42;
-},
-	// This method has interaction with the Native side
-testmethod: function() {
-	cordova.exec(
-				 function(){alert("success calling testmethod");},
-				 function(error){alert("error calling testmethod: " + error);},
-				 "UDPTransmit",
-				 "testMethod",
-				 []);
-	return true;
-},
-	// This method has interaction with the Native side and has parameters
-testmethodwithargs: function(x, y) {
-	cordova.exec(
-				 function(){alert("success calling testmethodwithargs");},
-				 function(error){alert("error calling testmethodwithargs: " + error);},
-				 "UDPTransmit",
-				 "testMethodWithArgs",
-				 [x, y]);
-	return true;
-},
-	
-	
-testmethodthatreturnsvalue: function(callback) {
-	cordova.exec(
-				 callback,
-				 function(error){alert("error calling testmethodthatreturnsvalue: " + error);},
-				 "UDPTransmit",
-				 "testmethodThatReturnsValue",
-				 []);
-	return true;
 }
-	
-	
 	
 };
 // End of cut and paste area

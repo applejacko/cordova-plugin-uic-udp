85c82a900aad5ec6e95d5a90bac4874a093a6d25
Jack Piccininni <applejacko@mac.com>
2014-07-02 14:21:51 -0500

    Added separate threads for iOS too.
    Added separate threads for the two methods for iOS too.

--- src/ios/UDPTransmit.m
+++ src/ios/UDPTransmit.m
@@ -32,46 +32,51 @@
 // Initializer for the packet and socket, takes a desination IP address and socket number
 - (void) initialize:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-	// Allocate the memory
-	memset(&broadcastAddr, 0, sizeof broadcastAddr);
-    broadcastAddr.sin_family = AF_INET;
-	
-	// Set the destination IP address
-	const char * ip_address = ((NSString *)[command.arguments objectAtIndex:0]).cString;
-	inet_pton(AF_INET, ip_address, &broadcastAddr.sin_addr); // Set the broadcast IP address
-	
-	// Set the destination port #
-	NSUInteger thePort = [[command.arguments objectAtIndex:1] integerValue];
-	broadcastAddr.sin_port = htons(thePort); // Set port, e.g., 4445
-	
-	// 	Create the socket
-	DatagramSocketC = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    int broadcastEnable=1;
-    setsockopt(DatagramSocketC, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
-	
-	NSString* socket = [NSString stringWithFormat:@"%i", DatagramSocketC];
-	if (DatagramSocketC != 0)
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[@"Success initializing UDP transmitter using datagram socket: " stringByAppendingString:socket]];
-	else
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Error initializing UDP transmitter using datagram socket: " stringByAppendingString:socket]];
-	
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+		CDVPluginResult* pluginResult = nil;
+		// Allocate the memory
+		memset(&broadcastAddr, 0, sizeof broadcastAddr);
+		broadcastAddr.sin_family = AF_INET;
+		
+		// Set the destination IP address
+		const char * ip_address = ((NSString *)[command.arguments objectAtIndex:0]).cString;
+		inet_pton(AF_INET, ip_address, &broadcastAddr.sin_addr); // Set the broadcast IP address
+		
+		// Set the destination port #
+		NSUInteger thePort = [[command.arguments objectAtIndex:1] integerValue];
+		broadcastAddr.sin_port = htons(thePort); // Set port, e.g., 4445
+		
+		// 	Create the socket
+		DatagramSocketC = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		int broadcastEnable=1;
+		setsockopt(DatagramSocketC, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+		
+		NSString* socket = [NSString stringWithFormat:@"%i", DatagramSocketC];
+		
+		if (DatagramSocketC != 0)
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[@"Success initializing UDP transmitter using datagram socket: " stringByAppendingString:socket]];
+		else
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Error initializing UDP transmitter using datagram socket: " stringByAppendingString:socket]];
+		
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	}];
 }
 
 // Sends a message to the IP and port set up in the initializer
 - (void) sendMessage:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-	messageToSend = ((NSString *)[command.arguments objectAtIndex:0]).cString;
-	ssize_t result = sendto(DatagramSocketC, messageToSend, strlen(messageToSend), 0, (struct sockaddr*)&broadcastAddr, sizeof broadcastAddr);
-	
-	if (result != 0)
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[@"Success transmitting UDP packet: " stringByAppendingString:[command.arguments objectAtIndex:0]]];
-	else
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Error transmitting UDP packet: " stringByAppendingString:[command.arguments objectAtIndex:0]]];
-	
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	[self.commandDelegate runInBackground:^{
+		CDVPluginResult* pluginResult = nil;
+		messageToSend = ((NSString *)[command.arguments objectAtIndex:0]).cString;
+		ssize_t result = sendto(DatagramSocketC, messageToSend, strlen(messageToSend), 0, (struct sockaddr*)&broadcastAddr, sizeof broadcastAddr);
+		
+		if (result != 0)
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[@"Success transmitting UDP packet: " stringByAppendingString:[command.arguments objectAtIndex:0]]];
+		else
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Error transmitting UDP packet: " stringByAppendingString:[command.arguments objectAtIndex:0]]];
+		
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	}];
 }
 
 @end

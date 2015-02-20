c498c50e185c16658e19ca817fa0c1730032ef9a
Jack Piccininni <applejacko@mac.com>
2015-02-20 10:55:23 -0600

    Adds last commit function for Android.

--- src/android/UDPTransmit.java
+++ src/android/UDPTransmit.java
@@ -53,68 +53,68 @@ public class UDPTransmit extends CordovaPlugin {
 			final int port = args.getInt(1);
 			// Run the UDP transmitter initialization on its own thread (just in case, see sendMessage comment)
 			cordova.getThreadPool().execute(new Runnable() {
-            	public void run() {
-            		this.initialize(host, port, callbackContext);
-            	}
-            	private void initialize(String host, int port, CallbackContext callbackContext) {
-            		boolean successResolvingIPAddress = false;
-            		// create packet
-            		InetAddress address = null;
-            		try {
-            			// 'host' can be a ddd.ddd.ddd.ddd or named URL, so doesn't always resolve
-            			address = InetAddress.getByName(host);
-            			successResolvingIPAddress = true;
-            		} catch (UnknownHostException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		}            		
-            		// If we were able to resolve the IP address from the host name, we're good to try to initialize
-            		if (successResolvingIPAddress) {
-            			byte[] bytes= new byte[0];
-                		datagramPacket = new DatagramPacket(bytes, 0, address, port);
-                		// create socket
-                		try {
-                			datagramSocket = new DatagramSocket();
-                			successInitializingTransmitter = true;
-    						
-                		} catch (SocketException e) {
-                			// TODO Auto-generated catch block
-                			e.printStackTrace();
-                		}      
-            		}  
-            		if (successInitializingTransmitter)
-            			callbackContext.success("Success initializing UDP transmitter using datagram socket: " + datagramSocket);
-            		else
-            			callbackContext.error("Error initializing UDP transmitter using datagram socket: " + datagramSocket);           			
-            	}
-            });
- 			return true;
+				public void run() {
+					this.initialize(host, port, callbackContext);
+				}
+				private void initialize(String host, int port, CallbackContext callbackContext) {
+					boolean successResolvingIPAddress = false;
+					// create packet
+					InetAddress address = null;
+					try {
+						// 'host' can be a ddd.ddd.ddd.ddd or named URL, so doesn't always resolve
+						address = InetAddress.getByName(host);
+						successResolvingIPAddress = true;
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					// If we were able to resolve the IP address from the host name, we're good to try to initialize
+					if (successResolvingIPAddress) {
+						byte[] bytes= new byte[0];
+						datagramPacket = new DatagramPacket(bytes, 0, address, port);
+						// create socket
+						try {
+							datagramSocket = new DatagramSocket();
+							successInitializingTransmitter = true;
+							
+						} catch (SocketException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					if (successInitializingTransmitter)
+						callbackContext.success("Success initializing UDP transmitter using datagram socket: " + datagramSocket);
+					else
+						callbackContext.error("Error initializing UDP transmitter using datagram socket: " + datagramSocket);
+				}
+			});
+			return true;
 		}
 		else if("sendMessage".equals(action)) {
 			final String message = args.getString(0);
 			// Run the UDP transmission on its own thread (it fails on some Android environments if run on the same thread)
 			cordova.getThreadPool().execute(new Runnable() {
-            	public void run() {
-            		this.sendMessage(message, callbackContext);
-            	}
- 				private void sendMessage(String data, CallbackContext callbackContext) {
-	 				boolean messageSent = false;
-	 				// Only attempt to send a packet if the transmitter initialization was successful
-	 				if (successInitializingTransmitter) {
- 						byte[] bytes = data.getBytes();
- 						datagramPacket.setData(bytes);
- 						try {
- 							datagramSocket.send(datagramPacket);
- 							messageSent = true;
- 						} catch (IOException e) {
- 							// TODO Auto-generated catch block
- 							e.printStackTrace();
- 						}
- 					}
+				public void run() {
+					this.sendMessage(message, callbackContext);
+				}
+				private void sendMessage(String data, CallbackContext callbackContext) {
+					boolean messageSent = false;
+					// Only attempt to send a packet if the transmitter initialization was successful
+					if (successInitializingTransmitter) {
+						byte[] bytes = data.getBytes();
+						datagramPacket.setData(bytes);
+						try {
+							datagramSocket.send(datagramPacket);
+							messageSent = true;
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
 					if (messageSent)
 						callbackContext.success("Success transmitting UDP packet: " + datagramPacket);
 					else
-						callbackContext.error("Error transmitting UDP packet: " + datagramPacket);												
+						callbackContext.error("Error transmitting UDP packet: " + datagramPacket);
 				}
 			});
 			return true;
@@ -123,24 +123,51 @@ public class UDPTransmit extends CordovaPlugin {
 			final String url = args.getString(0);
 			// Run the UDP transmission on its own thread (it fails on some Android environments if run on the same thread)
 			cordova.getThreadPool().execute(new Runnable() {
-            	public void run() {
-            		this.resolveHostName(url, callbackContext);
-            	}
- 				private void resolveHostName(String url, CallbackContext callbackContext) {
-	 				boolean hostNameResolved = false;
-            		InetAddress address = null;
-            		try {
-            			// 'host' can be a ddd.ddd.ddd.ddd or named URL, so doesn't always resolve
-            			address = InetAddress.getByName(url);
-            			hostNameResolved = true;
-            		} catch (UnknownHostException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		}            		
+				public void run() {
+					this.resolveHostName(url, callbackContext);
+				}
+				private void resolveHostName(String url, CallbackContext callbackContext) {
+					boolean hostNameResolved = false;
+					InetAddress address = null;
+					try {
+						// 'host' can be a ddd.ddd.ddd.ddd or named URL, so doesn't always resolve
+						address = InetAddress.getByName(url);
+						hostNameResolved = true;
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					if (hostNameResolved)
 						callbackContext.success(address.getHostAddress());
 					else
-						callbackContext.error("Error resolving host name: " + url);												
+						callbackContext.error("Error resolving host name: " + url);
+				}
+			});
+			return true;
+		}
+		else if("resolveHostNameWithUserDefinedCallbackString".equals(action)) {
+			final String url = args.getString(0);
+			final String userString = args.getString(1);
+			// Run the UDP transmission on its own thread (it fails on some Android environments if run on the same thread)
+			cordova.getThreadPool().execute(new Runnable() {
+				public void run() {
+					this.resolveHostNameWithUserDefinedCallbackString(url, userString, callbackContext);
+				}
+				private void resolveHostNameWithUserDefinedCallbackString(String url, String userString, CallbackContext callbackContext) {
+					boolean hostNameResolved = false;
+					InetAddress address = null;
+					try {
+						// 'host' can be a ddd.ddd.ddd.ddd or named URL, so doesn't always resolve
+						address = InetAddress.getByName(url);
+						hostNameResolved = true;
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if (hostNameResolved)
+						callbackContext.success(address.getHostAddress() + "|" + userString);
+					else
+						callbackContext.error(address.getHostAddress() + "|" + userString);
 				}
 			});
 			return true;
